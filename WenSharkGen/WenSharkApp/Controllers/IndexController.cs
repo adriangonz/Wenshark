7626cd151e5fa9f9685769d5ae01f36006a8f94b
kaseyo23 <kaseyo23@gmail.com>
2013-04-08 21:17:56 +0200

    Redirigido '/' a index2.html. He tenido que modificar los rutas de los recursos estaticos en el HTML (hace falta ponerlo entero, desde /)

---
+++ WenSharkGen/WenSharkApp/Controllers/IndexController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WenSharkApp.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            //return Redirect(Url.Content("/Assets/index2.html"));
+            Server.Transfer("/Assets/index2.html", true);
+
+            return null;
+        }
+
+    }
+}
