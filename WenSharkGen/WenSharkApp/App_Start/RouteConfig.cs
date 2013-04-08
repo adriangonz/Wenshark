2fe2e241942465c35cdf1edce828c854c064e7bc
kaseyo23 <kaseyo23@gmail.com>
2013-04-08 15:43:17 +0200

    Subo los recursos estaticos

--- WenSharkGen/WenSharkApp/App_Start/RouteConfig.cs
+++ WenSharkGen/WenSharkApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace WenSharkApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("Assets/{*pathInfo}");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
