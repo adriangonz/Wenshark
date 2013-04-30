58cee719ab5231155d3db374c812ba7ef72b10b0
kaseyo23 <kaseyo23@gmail.com>
2013-04-30 09:14:08 +0200

    Anyado el esqueleto de los controladores para los album y los artistas

---
+++ WenSharkGen/WenSharkApp/Controllers/AlbumController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WenSharkGenNHibernate.CEN.Default_;
+using WenSharkGenNHibernate.EN.Default_;
+
+namespace WenSharkApp.Controllers
+{
+    public class AlbumController : ApiController
+    {
+        public AlbumEN getAlbum(int id) 
+        {
+            //Obtenemos el album
+            AlbumCEN albumcen = new AlbumCEN();
+            AlbumEN album = albumcen.ReadOID(id);
+
+            if (album == null) throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+
+            //Esto no ira por culpa de los lazy y tal
+
+            return album;
+        }
+    }
+}
---
+++ WenSharkGen/WenSharkApp/Controllers/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WenSharkGenNHibernate.CEN.Default_;
+using WenSharkGenNHibernate.EN.Default_;
+
+namespace WenSharkApp.Controllers
+{
+    public class ArtistController : ApiController
+    {
+        public ArtistEN getArtist(int i)
+        {
+            //Obtenemos el artista por su ID
+            ArtistCEN artistcen = new ArtistCEN();
+            ArtistEN artist = artistcen.ReadOID(i);
+
+            if (artist == null) throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+
+            //Esto petara porque no hay aun sesiones ni mierdas
+
+            return artist;
+        }
+    }
+}
