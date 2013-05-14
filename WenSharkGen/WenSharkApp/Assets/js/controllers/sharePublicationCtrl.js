b815c9043d07c18fd8528577e3044c1e0b2e052f
wen96 <yosoyruben@gmail.com>
2013-05-14 17:09:29 +0200

    Terminadas las publicaciones

--- WenSharkGen/WenSharkApp/Assets/js/controllers/sharePublicationCtrl.js
+++ WenSharkGen/WenSharkApp/Assets/js/controllers/sharePublicationCtrl.js
@@ -1,17 +1,24 @@
 ﻿function sharePublicationCtrl($scope, $routeParams, $http) {
     $scope.sharing = false;
+    $scope.publication = {}
     $scope.share = function () {
         $scope.sharing = true;
+        
+        $scope.publication.item = $scope.itemClickedToPublish;
+        if (!$scope.publication.text) {
+            $scope.publication.text = "";
+        }
         var data = { description: $scope.publication.text, item: $scope.publication.item }
         $http
            .post('/api/Publication', JSON.stringify(data))
            .success(function (data) {
                $scope.sharing = false;
-               console.log(data);
+               $('#modalNewPublication').foundation('reveal', 'close');
            })
            .error(function (data) {
                console.log("Error compartiendo");
-                $scope.sharing = false;
+               $scope.sharing = false;
+               $('#modalNewPublication').foundation('reveal', 'close');
            });
     }
 }
